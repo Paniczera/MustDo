b2f1666bc88b83aa98c55ccac05160a65949d29b
Pedro Vinícius dos Santos Silva <vinisantos20011@hotmail.com>
2026-04-15 00:04:21 -0300

    feat: adiciona o registro da tarefa na tela

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const botao = document.querySelector('#botao');
 const input = document.querySelector('#inputTarefa');
+const tarefa1 = document.querySelector('#tarefa1')
 
 botao.onclick = function() {
     console.log(input.value);
+    tarefa1.innerText = input.value;
     input.value = '';
     input.placeholder = 'Tarefa Adicionada!';
 
     setTimeout(function() {
         input.placeholder = 'Digite uma tarefa';
-    }, 2000);
-    
-
+    }, 2000);  
 }
+
